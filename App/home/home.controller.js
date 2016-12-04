a46500431450ee26d4f39eeaacefc0f75df61dec
Dion MacIntyre <dion@macintyres.me.uk>
2016-12-04 16:07:02 +0000

    Improvements to the home page

--- App/home/home.controller.js
+++ App/home/home.controller.js
@@ -9,10 +9,20 @@
         vm.searchAllPhotos = function(){
             if(vm.photoQuery){
                 photoShareAPI.searchAllPhotos(vm.photoQuery).then(function(data){
-                    vm.photos = data;
+                    if(data.length < 1){
+                        //No photos found
+                    } else{
+                        vm.photos = data;
+                        $('body').animate({scrollTop:$('#imageContainer').offset().top - 50}, '500');
+                    }
                 });
             }
         };
+
+        vm.explore = function(){
+            //scroll to the top of imageContainer and remove 50px (for header)
+            $('body').animate({scrollTop:$('#imageContainer').offset().top - 50}, '500');
+        }
 
     });
 })();
